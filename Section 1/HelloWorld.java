adf77c89de310246e117c3cf792d360732d1c5fb
risingh1981 <67079298+risingh1981@users.noreply.github.com>
2020-06-18 18:46:34 -0500

    Update HelloWorld.java

--- Section 1/HelloWorld.java
+++ Section 1/HelloWorld.java
@@ -1,10 +1,19 @@
 /* *****************************************************************************
- *  Name:              Ricky Singh
+ *  Name:   Ricky Singh
+ *  Class:  Princeton: Computer Science: Programming with a Purpose
  *
+ *  Compilation: javac HelloWorld.java
+ *  Execution: java HelloWorld
+ *
+ *  Purpose: Prints "Hello, World"
+ *
+ *  % java HelloWorld
+ *  Hello, World
  **************************************************************************** */
 
 public class HelloWorld {
     public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
     }
 }
