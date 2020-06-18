c26aeea807fa24d899ed0a8629684a94032f4507
risingh1981 <67079298+risingh1981@users.noreply.github.com>
2020-06-18 18:44:13 -0500

    Update GreatCircle.java

--- Section 1/GreatCircle.java
+++ Section 1/GreatCircle.java
@@ -1,12 +1,23 @@
 /* *****************************************************************************
- *  Name:              Ricky Singh
- *  Floating-point numbers and the Math library. The great-circle distance is
- * the length of the shortest path between two points (x1,y1) and (x2,y2) on
- * the surface of a sphere, where the path is constrained to be along the surface.
- * Write a program GreatCircle.java that takes four double command-line arguments
- * x1, y1, x2, and y2—the latitude and longitude (in degrees) of two points on
- * the surface of the earth—and prints the great-circle distance (in kilometers)
- * between them. Use the following Haversine formula
+ *  Name:   Ricky Singh
+ *  Class:  Princeton: Computer Science: Programming with a Purpose
+ *
+ *  Compilation: javac GreatCircle.java
+ *  Execution: java GreatCircle x1 y1 x2 y2
+ *
+ *  Purpose: The great-circle distance is the length of the shortest path
+ *  between two points (x1,y1) and (x2,y2) on the surface of a sphere, where the
+ *  path is constrained to be along the surface. This program takes four double
+ *  command-line arguments x1, y1, x2, and y2—the latitude and longitude
+ *  (in degrees) of two points on the surface of the earth—and prints the
+ *  great-circle distance (in kilometers) between them using the Haversine
+ *  formula.
+ *
+ *  % java GreatCircle 40.35 74.65 48.87 -2.33
+ *  5902.927099258561 kilometers
+ *
+ *  java GreatCircle 60.0 15.0 120.0 105.0
+ *  4604.53989281927 kilometers
  **************************************************************************** */
 
 public class GreatCircle {
@@ -22,6 +33,7 @@ public class GreatCircle {
         double radx2 = Math.toRadians(x2);
         double rady2 = Math.toRadians(y2);
 
+        // Radius of Earth in Kilometers.
         double r = 6371.0;
 
         double sqrt = Math.pow(Math.sin((radx2 - radx1) / 2), 2) + (Math.cos(radx1) * Math
